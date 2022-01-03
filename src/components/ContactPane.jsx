4f78cdbcc1599f75aea8f061544023f988193936
NageebSalma <sleem.eaas@gmail.com>
2022-01-03 07:42:27 +0200

    mobile responsiveness improved

--- src/components/ContactPane.jsx
+++ src/components/ContactPane.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 const ContactPane = () => {
-    return <div className="bioPane contactPane">
+    return <div className=" contactPane">
     <h1>
         Contact me
     </h1>
