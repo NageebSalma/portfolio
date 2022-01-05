8639da6b4a770ac8009514d3e37e414759e451b4
NageebSalma <sleem.eaas@gmail.com>
2022-01-05 08:17:41 +0200

    0 opacity to divs

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+
 const Background = (props) => {
   return (
     <div className="background-container">
       <img
         className="background-img"
         alt="pigeons flying"
-        src="https://cdn.pixabay.com/photo/2021/11/01/21/31/forest-6761846_1280.jpg"      />
+        src="https://cdn.pixabay.com/photo/2021/11/01/21/31/forest-6761846_1280.jpg" 
+        style={{opacity:0.4}}     />
     </div>
   );
 };
